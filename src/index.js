91c0aec565203764cae21e40d88854fb59f525c4
Toshu Amashu <toshude@yahoo.de>
2021-06-26 18:23:49 +0200

    prettier-reformat

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+
 import App from './components/App';
 
 render(<App />, document.getElementById('app'));
 
 if (module.hot) {
-  module.hot.accept(function() {
-    window.location.reload();
-  });
+    module.hot.accept(function () {
+        window.location.reload();
+    });
 }
